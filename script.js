9d60ba97f9f7bd1cb2a42ed10cc47bb5d4a46984
mohamed110199 <144586606+mohamed110199@users.noreply.github.com>
2023-09-12 14:42:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let firstName = "Mohamed";
+let lastName = "Fathy";
+let age = 26;
+document.getElementById("first-name").textContent = firstName;
+document.getElementById("last-name").textContent = lastName;
+document.getElementById("age").textContent = age;
+console.error("Done");
